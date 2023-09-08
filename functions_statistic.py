6f49d9f16c1b002b3206a90a19a2ed87bdb4432e
mandauhitam <erfan_huda@rocketmail.com>
2023-09-08 21:30:41 +0700

    Update file statistic

--- functions_statistic.py
+++ functions_statistic.py
@@ -6,45 +6,6 @@ from statsmodels.tsa.stattools import adfuller
 from statsmodels.tsa.arima.model import ARIMA
 import matplotlib.pyplot as plt
 
-from abc import ABC, abstractmethod
-from datetime import datetime
-from typing import Literal
-
-def from_dict(data: dict) -> pd.DataFrame:
-    """Support data source from python dictionary"""
-    df = pd.DataFrame.from_dict(data=data)
-    return df
-
-def from_list(data: list) -> pd.DataFrame:
-    """Support data source from python list"""
-    df = pd.DataFrame(data=data)
-    return df
-
-def from_csv(file:any) -> pd.DataFrame:
-    """Support data source from CSV file"""
-    df = pd.read_csv(filepath_or_buffer=file, index_col="pt_date", parse_dates=True)
-    return df
-
-def from_excel(file:any) -> pd.DataFrame:
-    """Support data source from Excel file"""
-    df = pd.read_excel(filepath_or_buffer=file)
-    return df
-
-def from_sql(sql:any, con:str) -> pd.DataFrame:
-    """Support data source from SQL statements and its connection"""
-    df = pd.read_sql(sql=sql, con=con)
-    return df
-
-def from_sas(file:any, format: Literal["sas7bdat", "xport"]=None) -> pd.DataFrame:
-    """Support data source from SAS with default format sas7bdat"""
-    df = pd.read_sas(filepath_or_buffer=file,format=format)
-    return df
-
-def from_spss(file:any) -> pd.DataFrame:
-    """Support data source from SPSS"""
-    df = pd.read_spss(path=file)
-    return df
-
 def split_data(data: list, nrows=0) -> tuple:
     # Split dataset to train set and test set
     msk = (data.index < len(data)-nrows)
@@ -52,11 +13,6 @@ def split_data(data: list, nrows=0) -> tuple:
     df_test = data[~msk].copy()
 
     return (df_train, df_test)
-
-def do_log_transform(data):
-    """Transform data to log"""
-    data = np.log(data)
-    return data
 
 def do_data_interpolation(data):
     """Interpolate data in between"""
@@ -104,51 +60,160 @@ def forecast_arima(data, n=None):
 
     return data['forecast_auto']
 
-class InterfaceProcedure(ABC):
-    """Interface for statistical procedures and methodologies uses."""
-
-    @abstractmethod
-    def run():
-        """How data is output."""
-
-    @abstractmethod
-    def output_file():
-        """Where the output file stored"""
-
-class AutoArimaProcedure(InterfaceProcedure):
-    """Auto Arima Procedures"""
-    def __init__(self, data, split=0, output=None):
-        """Obtain source data from initializer"""
-        self.data = data
-        self.nrows = split
-        self.output = output
-        self.prediction = False
-        self.result = {}
-    
-    def output_file():
-        pass
-
-    def run(self):
-        data = from_csv(self.data)
-        train, test = split_data(data, nrows=self.nrows)
-        model, summary = auto_arima_test(train)
-        forecast = model.predict(n_periods=len(test))
-
-        self.result['rundate'] = datetime.now()
-        self.result['data'] = data 
-        self.result["train"] = train
-        self.result["test"] = test
-        self.result["model"] = model.to_dict()
-        self.result['prediction'] = [None] * len(train) + list(forecast)
-
-        print(summary)
-
-        return self
+""""""
+""" THE REAL IMPLEMENTATION OF SEABANK """
+""""""
 
 
-proc1 = AutoArimaProcedure("../file/input-python.csv", 3).run()
-# print(proc1.result['prediction'])
-print(proc1.result['model'])
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import scipy.special as sp
+import scipy.stats as stats
 
-pd.DataFrame(proc1.result['prediction']).to_csv("../file/output-python.csv", sep=",", header=False)
-# pd.DataFrame(proc1.result['model']).to_csv("../file/output-model.csv", sep=",", header=False)
+""" Implement the handling input of MEV variables and combine it. """
+proxy_odr = pd.read_excel("./file/test/ODR Tracking - OJK Buku 3.xlsx", sheet_name="OJK Historical ODR")
+odr = pd.read_csv("./file/input/mev/ODR.csv", index_col=["qoq_date", "pt_date","pd_segment", "tenor"])
+var1 = pd.read_csv("./file/input/mev/CPI_202306.csv", index_col="Date", parse_dates=True)
+var2 = pd.read_csv("./file/input/mev/GDP_202306.csv", index_col="Date", parse_dates=True)
+var3 = pd.read_csv("./file/input/mev/BI7D_202306.csv", index_col="Date", parse_dates=True)
+var4 = pd.read_csv("./file/input/mev/UNEMPLOYMENT_202306.csv", index_col="Date", parse_dates=True)
+var5 = pd.read_csv("./file/input/mev/USDIDR_202306.csv", index_col="Date", parse_dates=True)
+var6 = pd.read_csv("./file/input/mev/SGDIDR_202306.csv", index_col="Date", parse_dates=True)
+
+mev_combine = [var1, var2, var3, var4, var5, var6]
+
+""" Handling missing data """
+def fill_last_value(data):
+    return pd.concat(data).sort_values('Date').ffill().fillna(0)
+
+def fill_interpolate(data):
+    return pd.concat(data).sort_values('Date').interpolate().fillna(0)
+
+def fill_fwd_value(data):
+    return pd.concat(data).sort_values('Date').bfill().fillna(0)
+
+mev_combine = fill_interpolate(mev_combine)
+
+""" Handling MEV Extended Variables """
+def add_growth(data, k):
+    pass
+
+def add_delta(data, k):
+    pass
+
+def add_variances(data, k):
+    pass
+
+""" Handling plots """
+def generate_plots():
+    fig, ax = plt.subplots(nrows=6)
+    mev_combine['CPI'].plot(ax=ax[0], title=mev_combine['CPI'].name, color="green")
+    mev_combine['GDP'].plot(ax=ax[1], title=mev_combine['CPI'].name)
+    mev_combine['BI7DR'].plot(ax=ax[2], title=mev_combine["BI7DR"].name)
+    mev_combine['UNEMP'].plot(ax=ax[3], title=mev_combine["UNEMP"].name)
+    mev_combine['USDIDR'].plot(ax=ax[4], title=mev_combine["USDIDR"].name)
+    mev_combine['SGRIDR'].plot(ax=ax[5], title=mev_combine["SGRIDR"].name)
+
+    fig.savefig("./file/input/mev/mev_combine.pdf")
+    plt.show()
+
+
+"""Handling Proxy ODR"""
+
+fill_odr = proxy_odr.iloc[:].ffill()
+
+"""Handling ODR """
+group = odr.groupby(level=["pd_segment", "tenor"])
+odrs = [group.get_group(x) for x in group.groups]
+
+
+""" Handling ODR transformation"""
+def transform_zscore(odrs):
+    """ Z-Score for list of ODRS"""
+    for odr in odrs:
+        odr['zs_odr_balance'] = stats.zscore(odr['odr_balance'])
+        odr['zs_odr_loan'] = stats.zscore(odr['odr_loan'])
+        odr['zs_odr_client'] = stats.zscore(odr['odr_client'])
+    # return [x.apply(stats.zscore) for x in odrs]
+
+def transform_logit(odrs):
+    """ Logit for list of ODRS"""
+    for odr in odrs:
+        odr['Logit_odr_balance'] = odr['odr_balance'].apply(sp.logit)
+        odr['Logit_odr_loan'] = odr['odr_loan'].apply(sp.logit)
+        odr['Logit_odr_client'] = odr['odr_client'].apply(sp.logit)
+
+def transform_log10(odrs):
+    """Transform data to logarithmic base 10"""
+    for odr in odrs:
+        odr['ln_odr_balance'] = np.log10(odr['odr_balance'])
+        odr['ln_odr_loan'] = np.log10(odr['odr_loan'])
+        odr['ln_odr_client'] = np.log10(odr['odr_client'])
+
+def transform_log2(odrs):
+    """Transform data to logarithmic base 2"""
+    for odr in odrs:
+        odr['ln_odr_balance'] = np.log2(odr['odr_balance'])
+        odr['ln_odr_loan'] = np.log2(odr['odr_loan'])
+        odr['ln_odr_client'] = np.log2(odr['odr_client'])
+
+def transform_ln(odrs):
+    """Transform data to logarithmic natural"""
+    for odr in odrs:
+        odr['ln_odr_balance'] = np.log(odr['odr_balance'])
+        odr['ln_odr_loan'] = np.log(odr['odr_loan'])
+        odr['ln_odr_client'] = np.log(odr['odr_client'])
+
+def transform_sma(odrs, n=12):
+    """ Simple Moving Average for list of ODRS"""
+    for odr in odrs:
+        odr[f'SMA{n}_odr_balance'] = odr['odr_balance'].rolling(n).mean().fillna(0)
+        odr[f'SMA{n}_odr_loan'] = odr['odr_loan'].rolling(n).mean().fillna(0)
+        odr[f'SMA{n}_odr_client'] = odr['odr_client'].rolling(n).mean().fillna(0)
+
+def transform_cma(odrs, n=12):
+    """ Cumulative Moving Average for list of ODRS"""
+    for odr in odrs:
+        odr[f'CMA{n}_odr_balance'] = odr['odr_balance'].expanding(n).mean().fillna(0)
+        odr[f'CMA{n}_odr_loan'] = odr['odr_loan'].expanding(n).mean().fillna(0)
+        odr[f'CMA{n}_odr_client'] = odr['odr_client'].expanding(n).mean().fillna(0)
+
+def transform_ema(odrs, n=12):
+    """ Exponential Moving Average for list of ODRS"""
+    for odr in odrs:
+        odr[f'EMA{n}_odr_balance'] = odr['odr_balance'].ewm(span=n).mean().fillna(0)
+        odr[f'EMA{n}_odr_loan'] = odr['odr_loan'].ewm(span=n).mean().fillna(0)
+        odr[f'EMA{n}_odr_client'] = odr['odr_client'].ewm(span=n).mean().fillna(0)
+
+def transform_se(odrs):
+    """Simple Exponential for ODR"""
+    for odr in odrs:
+        odr['SE_odr_balance'] = odr['odr_balance'].apply(np.exp).fillna(0)
+        odr['SE_odr_loan'] = odr['odr_loan'].apply(np.exp).fillna(0)
+        odr['SE_odr_client'] = odr['odr_client'].apply(np.exp).fillna(0)
+
+# transform_zscore(odrs)
+
+""" Handling combination variables Between ODR and MEV"""
+def combine_variables(odrs, mev):
+    result = []
+    for odr in odrs:
+        result.append(pd.concat(odr, mev))
+
+    return result
+
+# variables = combine_variables(odrs, mev_combine).sort_values('Date')
+
+""" Handling ARIMA process"""
+
+
+# print(odrs)
+# odrs[2][['odr_balance','zs_odr_balance']].plot(label="ODR", figsize=(10,8))
+# plt.show()
+
+def export_odr(odrs):
+    """ Handling export to file """
+    for i in range(len(odrs)):
+        odrs[i].to_csv(f"./file/odr_python/py_odr_{odrs[i].index[0][2]}_{odrs[i].index[0][3]}.csv", mode="w")
+
+# export_odr(odrs)
