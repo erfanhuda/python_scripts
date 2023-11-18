0db9827b7316d5a8abc3085a42cc1083bd0371d0
mandauhitam <erfan_huda@rocketmail.com>
2023-11-18 10:19:59 +0700

    add cython

---
+++ cython_build.py
@@ -0,0 +1,16 @@
+import Cython
+import subprocess
+import os
+import sys
+
+PYTHON_VERSION = sys.version
+CYTHON_VERSION = Cython.__version__
+NODE_VERSION = subprocess.check_output(
+    [os.environ.get('node', 'node'), "--version"]).decode().splitlines()[0]
+# GCC_DETAIL_VERSION = [line for line in subprocess.check_output(
+#     [os.environ.get('CC', 'cc'), "--version", "-v"], stderr=subprocess.STDOUT).decode().splitlines() if 'version' in line][0]
+
+print("Python version :", PYTHON_VERSION)
+print("Node version :", NODE_VERSION)
+print("Cython version :", CYTHON_VERSION)
+# print(GCC_DETAIL_VERSION)
