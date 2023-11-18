484e4fb7d1db1cfca1a6a03df578e464d246cabe
mandauhitam <erfan_huda@rocketmail.com>
2023-11-18 09:35:00 +0700

    new_commit

--- csv_into_excel.py
+++ csv_into_excel.py
@@ -1,8 +1,15 @@
+import logging
 import pandas as pd
-import argparse 
-from pathlib import Path
+import time
+import argparse
+import os
+import glob
 
+start_time = time.time()
 parser = argparse.ArgumentParser()
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
 
 def convert_csv_to_excel(csv_file, excel_file):
     # Read the CSV file
@@ -11,12 +18,71 @@ def convert_csv_to_excel(csv_file, excel_file):
     # Write the DataFrame to an Excel file
     df.to_excel(excel_file, index=False, header=True)
 
+
 # Specify the paths for the CSV and Excel files
-parser.add_argument("--file", type=str, required=True)
+parser.add_argument("--file", type=str, required=False, default=None)
+parser.add_argument("--dir", type=str, required=False, default=None)
 args = parser.parse_args()
 
-csv_file_path = f'{args.file}.csv'
-excel_file_path = f'{args.file}.xlsx'
+dir_path = []
+excel_file_paths = []
 
-# Convert CSV to Excel
-convert_csv_to_excel(csv_file_path, excel_file_path)
+if args.file is None and args.dir is None:
+    logging.error("No file specified. CSV to Excel converter stopped.")
+
+elif args.file is not None and args.dir is None:
+    logging.info("Converting file %s" % f"{args.file}")
+
+    # Convert CSV to Excel
+    if args.file.endswith(".csv"):
+        csv_file_path = args.file
+        excel_file_path = ".".join([args.file.split(".csv")[0], "xlsx"])
+    else:
+        csv_file_path = f'{args.file}.csv'
+        excel_file_path = f'{args.file}.xlsx'
+
+    convert_csv_to_excel(csv_file_path, excel_file_path)
+
+    end_time = time.time()
+    logging.info(f"Execution duration {end_time - start_time} seconds")
+
+elif args.file is None and args.dir is not None:
+    logging.info("Converting directory %s" % f"{args.dir}")
+    # Convert multiple CSV
+    dir_path = glob.glob(os.path.join(args.dir, "*.csv"))
+    excel_file_paths = [".".join([file.split(".csv")[0], "xlsx"])
+                        for file in dir_path if file.endswith(".csv")]
+
+    for item in dir_path:
+        for output in excel_file_paths:
+            convert_csv_to_excel(item, output)
+
+    end_time = time.time()
+    logging.info(f"Execution duration {end_time - start_time} seconds")
+
+elif args.file is not None and args.dir is not None:
+    logging.info("Converting file {} and directory {}".format(
+        f"{args.file}", f"{args.dir}"))
+    # Convert CSV to Excel
+    if args.file.endswith(".csv"):
+        csv_file_path = args.file
+        excel_file_path = ".".join([args.file.split(".csv")[0], "xlsx"])
+    else:
+        csv_file_path = f'{args.file}.csv'
+        excel_file_path = f'{args.file}.xlsx'
+    # Convert multiple CSV
+    dir_path = glob.glob(os.path.join(args.dir, "*.csv"))
+    excel_file_paths = [".".join([file.split(".csv")[0], "xlsx"])
+                        for file in dir_path if file.endswith(".csv")]
+
+    for item in dir_path:
+        for output in excel_file_paths:
+            convert_csv_to_excel(item, output)
+
+    convert_csv_to_excel(csv_file_path, excel_file_path)
+
+    end_time = time.time()
+    logging.info(f"Execution duration {end_time - start_time} seconds")
+
+else:
+    raise ValueError("Could not run operation")
--- variable_transformation.py
+++ variable_transformation.py
@@ -3,12 +3,16 @@ import pandas as pd
 import glob
 import os
 
-variable_path = glob.glob(os.path.join(r"X:/dev/app/python_scripts/file/input/mev/", "*.csv"))
+SOURCE_PATH = "C:/Users/muhammad.huda/OneDrive - Seagroup/01. WORK/01. ECL/Remodelling FY 23/Q3/raw data/Up to Sep 2023/MEV/Wrapped/csv/worst"
+OUTPUT_PATH = "C:/Users/muhammad.huda/OneDrive - Seagroup/01. WORK/01. ECL/Remodelling FY 23/Q3/raw data/Up to Sep 2023/MEV/Wrapped/csv/output"
+OUTPUT_FILE = "py_MEV_WORST_transformation_Q3.xlsx"
+
+variable_path = glob.glob(os.path.join(SOURCE_PATH, "*.csv"))
 list_df = []
 
-# First CSV 
+# First CSV
 for item in variable_path:
-    df = pd.read_csv(item, parse_dates=['Date'],index_col=["Date"])
+    df = pd.read_csv(item, parse_dates=['Date'], index_col=["Date"])
     list_df.append(df)
 
 if len(list_df) > 1:
@@ -16,11 +20,24 @@ if len(list_df) > 1:
 else:
     frame = list_df[0]
 
-dt = pd.date_range(frame.index[0], frame.index[-1], freq="M").to_frame(name="Date")
+dt = pd.date_range(frame.index[0], frame.index[-1],
+                   freq="M").to_frame(name="Date")
 frame = pd.merge(dt, frame, left_index=True, right_index=True)
 frame = frame.drop(columns="Date").interpolate(method="linear")
 
 period = {
+    "1Q": 3,
+    "2Q": 6,
+    "3Q": 9,
+    "4Q": 12,
+    "1H": 6,
+    "2H": 12,
+    "3H": 18,
+    "4H": 24,
+    "1Y": 12,
+    "2Y": 24,
+    "3Y": 36,
+    "4Y": 48,
     "1M": 1,
     "2M": 2,
     "3M": 3,
@@ -32,54 +49,55 @@ period = {
     "9M": 9,
     "10M": 10,
     "11M": 11,
-    "12M": 12,
-    "1Q": 3,
-    "2Q": 6,
-    "3Q": 9,
-    "4Q": 12,
-    "1Y": 12,
-    "2Y": 24,
-    "3Y": 36,
-    "4Y": 48
+    "12M": 12
 }
 
 configuration = {
-    "lag" : {
-        "1M":"mom",
+    "lag": {
+        "1M": "mom",
         "3M": "qoq",
         "6M": "hoh",
+        "9M": "sos",
         "12M": "yoy",
     },
     "delta": {
-        "1Q": True,
-        "2Q": True,
-        "3Q": True,
-        "4Q": True,
+        "1M": True,
+        "3M": True,
+        "6M": True,
+        "9M": True,
+        "12M": True,
     },
     "growth": {
-        "1Q": True,
-        "2Q": True,
-        "3Q": True,
-        "4Q": True
+        "1M": True,
+        "3M": True,
+        "6M": True,
+        "9M": True,
+        "12M": True
     }
 }
 
-list_config = ["_".join([item, litem]) for item in configuration for litem in configuration[item] if configuration[item][litem]]
+list_config = ["_".join([item, litem])
+               for item in configuration for litem in configuration[item] if configuration[item][litem]]
 
 adv_config = itertools.combinations(list_config, r=2)
-adv_config = ["_".join([item[1], item[0].split("_")[1]]) for item in adv_config if "lag" in item[0] if "lag" not in item[1]]
+adv_config = ["_".join([item[1], item[0].split("_")[1]])
+              for item in adv_config if "lag" in item[0] if "lag" not in item[1]]
 
 list_config = list_config + adv_config
-adv_headers = ["_".join([item[0], item[1]]) for item in itertools.product(frame.columns.to_list(), list_config) ]
+adv_headers = ["_".join([item[0], item[1]]) for item in itertools.product(
+    frame.columns.to_list(), list_config)]
 adv_headers = pd.DataFrame(dt, columns=adv_headers)
 
 frame = pd.concat([frame, adv_headers], axis=1)
 
+
 def add_growth(x, y):
-    return (x / y) -1
+    return (x / y) - 1
+
 
 def add_delta(x, y):
     return x - y
+
 
 frame_config = []
 for col in frame:
@@ -95,24 +113,36 @@ for col in frame:
         frame_config.append(tuple([col, c[2]]))
 
         if c[2] != c[-1]:
-            # print(f"{c[0]}_lag_{c[-1]}")
-            frame[col] = frame[f"{c[0]}_lag_{c[-1]}"] - frame[f"{c[0]}_lag_{c[-1]}"].shift(period.get(c[2]))
+            frame[col] = frame[f"{c[0]}_lag_{c[2]}"] - \
+                frame[f"{c[0]}_lag_{c[2]}"].shift(period.get(c[-1]))
+
+            frame[col].rename(
+                "_".join([c[0], c[1], "lag", configuration['lag'][c[2]], c[3]]), inplace=True)
+
         else:
             # print(f"{c[0]}")
-            frame[col] = frame[f"{c[0]}"] - frame[f"{c[0]}"].shift(period.get(c[-1]))
+            frame[col] = frame[f"{c[0]}"] - \
+                frame[f"{c[0]}"].shift(period.get(c[-1]))
 
     elif "growth" in col:
         c = col.split("_")
         frame_config.append(tuple([col, c[2]]))
-        
+
         if c[2] != c[-1]:
             # print(f"{c[0]}_lag_{c[-1]}")
-            frame[col] = (frame[f"{c[0]}_lag_{c[-1]}"] / frame[f"{c[0]}_lag_{c[-1]}"].shift(period.get(c[2]))) - 1
-        else:
-            # print(f"{c[0]}")
-            frame[col] = (frame[f"{c[0]}"] / frame[f"{c[0]}"].shift(period.get(c[-1]))) - 1
+            frame[col] = (frame[f"{c[0]}_lag_{c[-1]}"] /
+                          frame[f"{c[0]}_lag_{c[-1]}"].shift(period.get(c[2]))) - 1
 
+            frame[col].rename(
+                "_".join([c[0], c[1], "lag", configuration['lag'][c[2]], c[3]]), inplace=True)
+        else:
+            frame[col] = (
+                frame[f"{c[0]}"] / frame[f"{c[0]}"].shift(period.get(c[-1]))) - 1
     else:
         frame_config.append(tuple([col, 0]))
 
-frame.to_excel("X:/dev/app/python_scripts/file/output/mev/combine_variable2.xlsx")
+frame.interpolate(method="linear").to_excel(
+    os.path.join(OUTPUT_PATH, OUTPUT_FILE))
+
+training_date = "2023-09-30"
+testing_date = "2023-10-31"
