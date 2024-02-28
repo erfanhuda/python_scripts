e516319bb7253cd5e2a17ec86670e15ae63caeb2
mandauhitam <erfan_huda@rocketmail.com>
2024-02-28 08:32:30 +0700

    OK editing

--- mev_arima.py
+++ mev_arima.py
@@ -1,2 +1,13 @@
-import pandas as pd
-import pmdarima as pm
+import blpapi
+
+
+class AuthBloomberg(blpapi.AuthUser):
+    def __init__(self, name, ip):
+        super().__init__(blpapi.AuthUser)
+        self.name = name
+        self.ip = ip
+        self.createWithLogonName(self.name, self.ip)
+
+
+def main():
+    auth = blpapi.AuthUser().createWithLogonName()
