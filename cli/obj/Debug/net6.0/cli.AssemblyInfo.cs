6ddf7f0699ba66fbfb8d0d4066c3ed9dc4e3cffd
erfanhuda <erfan_huda@rocketmail.com>
2024-03-23 20:50:34 +0700

    Ok

--- cli/obj/Debug/net6.0/cli.AssemblyInfo.cs
+++ cli/obj/Debug/net6.0/cli.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cli")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9aa3791317e951c372dcc22c650346d50058e8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05c53199617d4ffaf137c063721297f692eaf39a")]
 [assembly: System.Reflection.AssemblyProductAttribute("cli")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cli")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
