b913cc73c116a95d937b2af0f14e543dae7cdd2b
Erfan Huda <erfan_huda@rocketmail.com>
2024-06-21 16:03:47 +0700

    new typec

--- C/main.c
+++ C/main.c
@@ -4,3 +4,19 @@ void prompt(int name)
 {
     printf("%d", name);
 };
+
+int add(int num1, int num2)
+{
+    return num1 + num2;
+}
+
+struct point
+{
+    int x;
+    int y;
+};
+
+void Point(struct point p)
+{
+    printf("%d %d\n", p.x, p.y);
+}
--- C/main.py
+++ C/main.py
@@ -1,4 +1,20 @@
+import os
 import ctypes
 
-win = ctypes.CDLL(".\\main.so")
-win.prompt(3000)
+path = os.getcwd()
+clib = ctypes.CDLL(os.path.join(path, 'main.so'))
+
+
+x = clib.add
+x.argtypes = [ctypes.c_int, ctypes.c_int]
+x.restype = ctypes.c_int
+
+# clib.prompt(3000)
+# print(x(20, 1000))
+
+
+class Point(ctypes.Structure):
+    _fields_ = [("x", ctypes.c_int), ("y", ctypes.c_int)]
+
+p1 = Point(10, 20)
+point = clib.Point(p1)
